f17b8f3b43ab770e92545791df9b49a857b1560b
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-02-01 06:44:58 +0200

    components created ... all routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,35 @@
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
+import { SuggestionsComponent } from './suggestions/suggestions.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogsDetailsComponent } from './blogs-details/blogs-details.component';
 import { LocalHomeComponent } from './local-home/local-home.component';
+import { ProfileEditComponent } from './profile-edit/profile-edit.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
 
 const routes: Routes = [
-  {path:'home',component:LocalHomeComponent},
-  {path:'details/:id',component:BlogsDetailsComponent}
-
+  {
+    path: 'home', component: HomeComponent
+  },
+  { path: 'followings-blogs', component: HomeComponent },
+  {
+    path: 'author/:userid', component: ProfileComponent,
+    children: [
+      { path: 'edit-profile', component: ProfileEditComponent }
+    ]
+  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'suggestions', component: SuggestionsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'blog/:blogid', component: HomeComponent },
+  { path: 'create-blog', component: BlogCreateComponent },
+  { path: 'edit-blog/:blogid', component: BlogEditComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,42 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LocalHomeComponent } from './local-home/local-home.component';
 import { BlogsDetailsComponent } from './blogs-details/blogs-details.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { SuggestionsComponent } from './suggestions/suggestions.component';
+import { AboutComponent } from './about/about.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { AuthorCardComponent } from './author-card/author-card.component';
+import { LoginComponent } from './login/login.component';
+import { BlogCardComponent } from './blog-card/blog-card.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { SearchComponent } from './search/search.component';
+import { ProfileEditComponent } from './profile-edit/profile-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LocalHomeComponent,
-    BlogsDetailsComponent
+    BlogsDetailsComponent,
+    HomeComponent,
+    ProfileComponent,
+    RegisterComponent,
+    SuggestionsComponent,
+    AboutComponent,
+    NavigationComponent,
+    BlogsComponent,
+    AuthorCardComponent,
+    LoginComponent,
+    BlogCardComponent,
+    BlogDetailsComponent,
+    BlogCreateComponent,
+    BlogEditComponent,
+    SearchComponent,
+    ProfileEditComponent
   ],
   imports: [
     BrowserModule,
