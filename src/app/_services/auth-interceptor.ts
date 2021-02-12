b4a39207461629255dfe569128eb4abd29de81a5
ahmedatefmohamed <43454769+ahmedatefmohamed@users.noreply.github.com>
2021-02-12 18:25:51 +0200

    Auth-Interceptor Comment

--- src/app/_services/auth-interceptor.ts
+++ src/app/_services/auth-interceptor.ts
@@ -1,3 +1,7 @@
+// Auth-Interceptor//
+// The Basic Authentication Interceptor intercepts http requests from the application to add basic authentication
+// credentials to the Authorization header if the user is logged in.
+
 import { Observable } from 'rxjs';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -14,6 +18,7 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private auth : AuthenticationService) {
   }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // add authorization header with basic auth credentials if available
     req = req.clone({
       setHeaders: {
         'Content-Type' : 'application/json; charset=utf-8',
