1002144821b269703f67112f7905010a42f6b218
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-04-02 15:29:52 +0200

    register form validation fixed

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,8 +6,9 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 import { first } from 'rxjs/operators';
 
 import { UserService } from '../_services/user.service';
-import {AuthenticationService } from '../_services/authentication.service'
+import { AuthenticationService } from '../_services/authentication.service'
 import { AlertService } from '../_services/alert.service'
+import { MustMatch } from '../_helpers/MustMatch';
 
 
 @Component({
@@ -19,8 +20,8 @@ export class RegisterComponent implements OnInit {
   /** object defines the form controls and validators,
    * and is used to access data entered into the form
    * Construct a new FormGroup instance. Returns FormGroup
-  */  
-  registerForm: FormGroup = this.formBuilder.group({}); 
+  */
+  registerForm: FormGroup = this.formBuilder.group({});
   loading = false;
   submitted = false;
 
@@ -30,8 +31,8 @@ export class RegisterComponent implements OnInit {
     public auth: AuthenticationService,
     private userService: UserService,
     private alertService: AlertService
-  ) { 
-     
+  ) {
+
   }
   get fieldget() { return this.registerForm.controls; }
 
@@ -40,44 +41,48 @@ export class RegisterComponent implements OnInit {
      *  controlsConfig Object :
      * 	A collection of child controls. The key for each child is the name under which it is registered.
      */
-    this.registerForm = this.formBuilder.group({ 
+    this.registerForm = this.formBuilder.group({
       firstName: ['', [Validators.required, Validators.maxLength(140)]],
       lastName: ['', [Validators.required, Validators.maxLength(140)]],
       username: ['', [Validators.required, Validators.maxLength(140)]],
-      gender: [''],
+      gender: ['', Validators.required],
       jobTitle: ['', Validators.required],
-      email: ['',Validators.required],
-      // [Validators.required, Validators.pattern('/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/')]
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      
-  });
-  
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8),Validators.maxLength(140)]],
+      confirmPassword: ['', [Validators.required]]
+    },{
+        validator: MustMatch('password', 'confirmPassword')
+      }
+    );
   }
 
-    onSubmit(registerForm:FormGroup) {
-        this.submitted = true;
 
-        // reset alerts on submit
-        this.alertService.clear();
 
-        // stop here if form is invalid
-        if (registerForm.invalid) {
-            return;
-        }
 
-        this.loading = true; // loading Spinner= true.
-        console.log(registerForm.value);
-        this.userService.register(registerForm.value)
-            .pipe(first()) // first(): operator takes an optional predicate function and emits an error notification when no value matched when the source completed.
-            .subscribe(
-                data => {
-                    this.alertService.success('Registration successful', true);
-                    this.router.navigate(['/login']);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false; // loading Spinner= false.
-                });
+  onSubmit(registerForm: FormGroup) {
+    this.submitted = true;
+
+    // reset alerts on submit
+    this.alertService.clear();
+
+    // stop here if form is invalid
+    if (registerForm.invalid) {
+      return;
     }
+
+    this.loading = true; // loading Spinner= true.
+    console.log(registerForm.value);
+    this.userService.register(registerForm.value)
+      .pipe(first()) // first(): operator takes an optional predicate function and emits an error notification when no value matched when the source completed.
+      .subscribe(
+        data => {
+          this.alertService.success('Registration successful', true);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false; // loading Spinner= false.
+        });
+  }
 
 }
